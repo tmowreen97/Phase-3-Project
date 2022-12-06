df3ec5021fad822bea6cecbd4c3413af430c160b
Tasreen Mowreen <tmowreen97@gmail.com>
2022-12-05 21:06:41 -0500

    updated database, ran server

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@ class ApplicationController < Sinatra::Base
   
   # Add your routes here
   get "/" do
-    { message: "Good luck with your project!" }.to_json
+    movies = Movie.all
+    movies.to_json
   end
 
 end
